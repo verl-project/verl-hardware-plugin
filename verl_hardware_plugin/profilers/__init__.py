f48d18ca1c8d0e1fc3dfad2754b824fe585c0e4b
kelsieyuan <kelsieyuan@users.noreply.github.com>
2026-08-06 16:29:38 +0800

    update license
    Added copyright and license information to the file.

--- verl_hardware_plugin/profilers/__init__.py
+++ verl_hardware_plugin/profilers/__init__.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2026 BAAI. All rights reserved.
+# Licensed under the Apache License, Version 2.0.
+
 """Profiler integrations via monkey-patching verl's profiler utilities."""
 
 import logging
